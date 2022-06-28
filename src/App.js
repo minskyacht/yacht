7bff0daa76accba0b6435b7a5eadc5f97840d38e
Darya.Liashchynskaya <Dashalesh040600>
2022-06-28 21:03:06 +0300

    edit Header Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import {Header} from "./Header/Header";
 import {HeaderHat} from './HeaderHat/HeaderHat'
+import {ReservationContainer} from "./ReservationContainer/ReservationContainer";
+import {useWindowSize} from "./hooks/useWindowSize";
 
 function App() {
+ const {width,height}=useWindowSize()
 
   return (
-    <>
-        <HeaderHat />
-        <Header/>
-    </>
+    <div style={{backgroundColor:'white'}}>
+        <Header />
+        <HeaderHat windowHeight={height}/>
+        <ReservationContainer />
+    </div>
   );
 }
 
